3cce2f79c2dc8ad9c82c2337ff8dbaf083fa70f9
prachiji87 <prachi.tiwari@sustaininsight.com>
2025-11-25 12:07:22 +0530

    done

--- src/components/Pages/HeaderBanner.jsx
+++ src/components/Pages/HeaderBanner.jsx
@@ -4,12 +4,12 @@ const HeaderBanner = () => {
     return (
    
         
-            <div style={{ backgroundColor: '#e8d7cb' }}>
-                <marquee behavior="scroll" direction="left" scrollamount="10" className="text-sm font-medium tracking-wide">
-                   Serving Clients Across INDIA, UAE, UK 
+            <div style={{ backgroundColor: '#e8d7cb', height: '32px', display: 'flex', alignItems: 'center' }}>
+                <marquee behavior="scroll" direction="left" scrollamount="10" className=" tracking-wide">
+                   <h6 style={{ fontSize: '15px', fontWeight: 400, margin: 0, lineHeight: '32px' }}>Serving Clients Across INDIA, UAE, UK </h6>
                 </marquee>
             </div>
-      
+        
     );
 };
 
