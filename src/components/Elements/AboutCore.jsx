f4ccbe5349c9f3a76579aa88a4e1f98cadee36d0
prachiji87 <prachi.tiwari@sustaininsight.com>
2025-11-28 11:54:10 +0530

    solved

---
+++ src/components/Elements/AboutCore.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export default function AboutCore() {
+    const values = [
+        {
+            icon: 'flaticon-sketch',
+            title: 'Innovation',
+            description: 'Pushing boundaries with cutting-edge design solutions and creative approaches that transform spaces.'
+        },
+        {
+            icon: 'flaticon-stairs',
+            title: 'Excellence',
+            description: 'Uncompromising quality in every detail, from concept to execution, ensuring perfection in every project.'
+        },
+        {
+            icon: 'flaticon-window',
+            title: 'Sustainability',
+            description: 'Eco-conscious design practices that create beautiful spaces while respecting our environment.'
+        },
+        {
+            icon: 'flaticon-door',
+            title: 'Client Focus',
+            description: 'Your vision is our mission. We listen, understand, and deliver spaces that reflect your unique style.'
+        }
+    ];
+
+
+    
+    var bgimg1 = require('./../../images/background/cross-line2.png');
+    var bgimg2 = require('./../../images/background/bg-4.png');
+
+
+  return (
+    <div>
+          <div className="section-full mobile-page-padding p-t80 p-b80 bg-white" style={{ backgroundImage: 'url(' + bgimg2 + ')', backgroundRepeat: 'no-repeat', backgroundPosition: 'bottom left' }}>
+                    <div className="container">
+                        <div className="section-head text-center">
+                            <div className="sx-separator-outer separator-center">
+                                <div className="sx-separator bg-white bg-moving bg-repeat-x" style={{ backgroundImage: 'url(' + bgimg1 + ')' }}>
+                                <h3 style={{ border: 'none' , padding:"10px", }} className="">What Makes Us Different</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section-content m-t50">
+                            <div className="row">
+                                {values.map((item, index) => (
+                                    <div className="col-lg-3 col-md-6 col-sm-12 m-b30" key={index}>
+                                        <div className="sx-icon-box-wraper icon-count-2-outer">
+                                            <div className="icon-count-2 bg-white">
+                                                <div className="icon-xl inline-icon m-b15 scale-in-center">
+                                                    <span className="icon-cell"><i className={item.icon} /></span>
+                                                </div>
+                                                <div className="icon-content">
+                                                    <h4 className="sx-tilte">{item.title}</h4>
+                                                    <p>{item.description}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
+  )
+}
--- src/components/Pages/AboutSection.jsx
+++ src/components/Pages/AboutSection.jsx
@@ -7,37 +7,13 @@ import bnrimg from './../../images/banner/5.jpg';
 import FooterSection from './../Common/FooterSection';
 import AboutStory from '../Elements/AboutStory';
 import AboutMission from '../Elements/AboutMission';
+import AboutCore from '../Elements/AboutCore';
 
-var bgimg1 = require('./../../images/background/cross-line2.png');
-var bgimg2 = require('./../../images/background/bg-4.png');
-var bgimg3 = require('./../../images/background/bg-1.jpg');
-var bgimg4 = require('./../../images/background/bg-5.png');
+var bgimg3 = require('./../../images/background/cross-line2.png');
+var bgimg4 = require('./../../images/background/pic.jpg');
 
 
 const AboutSection = () => {
-    const values = [
-        {
-            icon: 'flaticon-sketch',
-            title: 'Innovation',
-            description: 'Pushing boundaries with cutting-edge design solutions and creative approaches that transform spaces.'
-        },
-        {
-            icon: 'flaticon-stairs',
-            title: 'Excellence',
-            description: 'Uncompromising quality in every detail, from concept to execution, ensuring perfection in every project.'
-        },
-        {
-            icon: 'flaticon-window',
-            title: 'Sustainability',
-            description: 'Eco-conscious design practices that create beautiful spaces while respecting our environment.'
-        },
-        {
-            icon: 'flaticon-door',
-            title: 'Client Focus',
-            description: 'Your vision is our mission. We listen, understand, and deliver spaces that reflect your unique style.'
-        }
-    ];
-
     const process = [
         {
             number: '01',
@@ -72,36 +48,7 @@ const AboutSection = () => {
 
              <AboutMission />
 
-                <div className="section-full mobile-page-padding p-t80 p-b80 bg-white" style={{ backgroundImage: 'url(' + bgimg2 + ')', backgroundRepeat: 'no-repeat', backgroundPosition: 'bottom left' }}>
-                    <div className="container">
-                        <div className="section-head text-center">
-                            <div className="sx-separator-outer separator-center">
-                                <div className="sx-separator bg-white bg-moving bg-repeat-x" style={{ backgroundImage: 'url(' + bgimg1 + ')' }}>
-                                    <h3 className="sep-line-one">Our Core Values</h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="section-content m-t50">
-                            <div className="row">
-                                {values.map((item, index) => (
-                                    <div className="col-lg-3 col-md-6 col-sm-12 m-b30" key={index}>
-                                        <div className="sx-icon-box-wraper icon-count-2-outer">
-                                            <div className="icon-count-2 bg-white">
-                                                <div className="icon-xl inline-icon m-b15 scale-in-center">
-                                                    <span className="icon-cell"><i className={item.icon} /></span>
-                                                </div>
-                                                <div className="icon-content">
-                                                    <h4 className="sx-tilte">{item.title}</h4>
-                                                    <p>{item.description}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <AboutCore />
 
                 {/* <div className="section-full overlay-wraper sx-bg-secondry mobile-page-padding p-t80 p-b50 bg-parallax" data-stellar-background-ratio="0.5" style={{ backgroundImage: 'url(' + bgimg3 + ')' }}>
                     <div className="overlay-main bg-black opacity-0.10" />
