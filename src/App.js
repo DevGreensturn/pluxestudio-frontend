3b4b4460df264df76d9e180069de0c3f8d8b1ed1
prachiji87 <prachi.tiwari@sustaininsight.com>
2026-02-11 17:34:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Components from './components/Components';
 import ScrolToTop from './components/Elements/ScrolToTop';
 import Loader from "./components/Elements/Loader";
-
+ 
 const App = () => {
   
   return (
